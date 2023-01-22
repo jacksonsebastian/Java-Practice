c5d499daef738ab52634f99c31c2406e8220b6d9
Jackson <technologyj4ck@gmail.com>
2023-01-23 04:48:37 +0530

    Fourth commit

---
+++ Encapsulation/encapsulation.java
@@ -0,0 +1,9 @@
+public class encapsulation {
+    public static void main(String[] args) {
+        person obj = new person();
+        obj.setname("Arthi");
+        obj.setRollno(1);
+        System.out.println(obj.getname() + " " + obj.getRollno());
+    }
+}
+
---
+++ Encapsulation/person.java
@@ -0,0 +1,24 @@
+public class person {
+    //Access modifier
+    private String name;
+    private int rollno;
+
+    //Getter
+
+
+    public int getRollno(){
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String newName) {
+        this.name = newName;
+    }
+}
